08334a6982043d454f0a004615e9ac18e8118caa
RedbirdXVO <63523784+RedbirdXVO@users.noreply.github.com>
2022-12-11 23:29:42 -0500

    changes

--- models/User.js
+++ models/User.js
@@ -14,6 +14,10 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please enter a password'],
     minlength: [6, 'Minimum password length is 6 characters'],
+  },
+  isInstructor: {
+    type: Boolean,
+    default: false
   }
 });
 
